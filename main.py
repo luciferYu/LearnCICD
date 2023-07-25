9dc1a9e03fd8cbf7822441030197e7dba2f49e45
yuzhiyi <yuzhiyi@isid.com.cn>
2023-07-25 14:10:47 +0800

    ssh test

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#          ssh-keyscan -t rsa ${{ secrets.DEPLOY_HOST_IP }} >> ~/.ssh/known_hosts
-#          chmod 644 ~/.ssh/known_hosts
-#
 # environment: production
 # branches: [ $default-branch ]
 print('hello')
